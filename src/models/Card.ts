432c43d7806033dc2c724e401f3ed7cfd7f67471
nikuz <micurino@gmail.com>
2021-01-08 16:18:38 +0100

    fix cards url generation

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -1,5 +1,6 @@
 
 import { Color } from '../types';
+import appParams from '../../package.json';
 
 interface Props {
     id: string,
@@ -13,8 +14,8 @@ export default class Card {
         this.id = props.id;
         this.rank = props.rank;
         this.rankId = props.rankId;
-        this.front = `/cards/${props.id}.png`;
-        this.back = `/cards/${props.color}_back.png`;
+        this.front = `/${appParams.name}/cards/${props.id}.png`;
+        this.back = `/${appParams.name}/cards/${props.color}_back.png`;
         this.color = props.color;
     }
 
